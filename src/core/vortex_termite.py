8f2742187b4e676c36f2f6019e6cdb47d8c0f9e4
Lyle Antoine <lyleantoine@gmail.com>
2025-11-13 20:17:08 -0330

    Create vortex_termite.py

---
+++ src/core/vortex_termite.py
@@ -0,0 +1,11 @@
+# src/core/vortex_termite.py
+import numpy as np
+
+def apply_termite_decay(tubes, decay_rate=0.95):
+    return tubes * decay_rate
+
+def apply_vortex_dynamics(tubes, path, strength=0.3):
+    for y, x in path:
+        if np.random.rand() < strength:
+            tubes[max(0,y-1):y+2, max(0,x-1):x+2] *= 1.5
+    return tubes
