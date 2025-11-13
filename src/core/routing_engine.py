a25eb83e0d05f2e5142d270a5681b030453e014a
Lyle Antoine <lyleantoine@gmail.com>
2025-11-13 07:29:36 -0330

    Create routing_engine.py

---
+++ src/core/routing_engine.py
@@ -0,0 +1,5 @@
+# src/core/routing_engine.py
+from src.core.factory import get_optimizer
+def design_route(dem_path, start_latlon, end_latlon, rules, module="canal"):
+    # load DEM, run optimizer with rules, return path + cost + kml
+    pass  # full logic uses all above pieces
