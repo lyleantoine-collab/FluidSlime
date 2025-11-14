0d170cbe0f58893ca068a98d5808d3ec776c2402
Lyle Antoine <lyleantoine@gmail.com>
2025-11-13 21:01:35 -0330

    Update merge.py

--- src/core/merge.py
+++ src/core/merge.py
@@ -1,11 +1,14 @@
-# src/core/merge.py
+import numpy as np
+
 def merge_paths(paths, rules):
     merged = []
     for path in paths:
-        if not any(np.array_equal(path[-1], m[0]) for m in merged):
+        attached = False
+        for m in merged:
+            if np.array_equal(path[-1], m[0]):
+                m.extend(path[1:])
+                attached = True
+                break
+        if not attached:
             merged.append(path)
-        else:
-            for m in merged:
-                if np.array_equal(path[-1], m[0]):
-                    m.extend(path[1:])
     return merged
